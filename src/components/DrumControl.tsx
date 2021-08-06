96b82476861009c29469d271737cdd09690c9a54
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-08-06 11:54:40 +0800

    range slider style

--- src/components/DrumControl.tsx
+++ src/components/DrumControl.tsx
@@ -8,7 +8,8 @@ export const DrumControl = () => {
       </div>
       <div className="mt-2 h-8 bg-grey w-3/5 mx-auto"></div>
       <div className="mt-4">
-        <input type="range" min="0" max="1" step="0.0.1" value="0.64"/>
+        <input type="range" min="0" max="1" step="0.0.1" value="0.64"
+        className="appearance-none w-3/4 cursor-pointer bg-charcoal h-2 shadow-sm-lr"/>
       </div>
       <div className="mt-2">
         <span className="text-xs">Bank</span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         'suva-grey': '#8d8d8d',
         'dark-gray': '#b3b3b3',
         'grey': '#808080',
+        'charcoal': '#424242',
       },
     },
     fontFamily: {
@@ -14,7 +15,8 @@ module.exports = {
     },
     boxShadow: {
       '2xl-lrb': '0px 10px 11px 4px rgba(0, 0, 0, 0.25)',
-      'xl-rb': '3px 5px 5px 0px rgba(0,0,0,0.75);',
+      'xl-rb': '3px 5px 5px 0px rgba(0,0,0,0.75)',
+      'sm-lr': '-1px 1px 2px 1px rgba(0,0,0,0.75), 1px 1px 2px 1px rgba(0,0,0,0.75)',
     }
   },
   variants: {
