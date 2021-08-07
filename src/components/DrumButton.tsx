4f401cf06d9462fc9c4bc45728b6fb30e8952710
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-08-07 15:03:57 +0800

    display id

--- src/components/DrumButton.tsx
+++ src/components/DrumButton.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react"
-import { useAppSelector } from "../app/hooks"
+import { useAppDispatch, useAppSelector } from "../app/hooks"
 import { BankPayload } from "../features/bank/bankType"
+import { changeDescription } from "../features/description/descriptionSlice"
 import { selectVolume } from "../features/volumeSlice"
 
 export const DrumButton = (props: DrumButtonProps) => {
   const volume = useAppSelector(selectVolume)
+  const dispatch = useAppDispatch()
 
   const playAudio = () => {
     const audio = new Audio(props.bank.url)
@@ -12,10 +14,15 @@ export const DrumButton = (props: DrumButtonProps) => {
     audio.play()
   }
 
+  const displayId = () => {
+    dispatch(changeDescription(props.bank.id.split('-').join(' ')))
+  }
+
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       if (e.key === props.bank.keyTrigger.toLowerCase()) {
         playAudio()
+        displayId()
       }
     }
     window.addEventListener('keydown', handleKeyDown)
@@ -24,9 +31,14 @@ export const DrumButton = (props: DrumButtonProps) => {
     }
   })
 
+  const handleClick = () => {
+    playAudio()
+    displayId()
+  }
+
   return (
     <div
-      onClick={playAudio}
+      onClick={handleClick}
       className="h-14 w-16 p-4 text-center bg-grey rounded shadow-xl-rb">
       <span>{props.children}</span>
     </div>
